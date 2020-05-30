8dede5f0d9048aca8c4681beb66aea8393c6b961
Satria Wicaksana Adhipurusa <satriawicaksanaadhipurusa@gmail.com>
2020-05-30 17:55:05 +0700

    Restructure app.py

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
-api = Api(app)
-
-
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
 # /auth send username and password
 # authenticate function will check and return a user
 # the endpoint send back a JWT token
 # when we send this token through API
 # JWT will call the identity function and get user by id
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_AUTH_URL_RULE'] = '/login'
-jwt = JWT(app, authenticate, identity_function)
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+app.secret_key = 'jose'
+
+api = Api(app)
+jwt = JWT(app, authenticate, identity_function)
+db.init_app(app)
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 
 @jwt.auth_response_handler
@@ -46,6 +46,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
+
 if __name__ == "__main__":
-    db.init_app(app)
     app.run(port=5000, debug=True)
